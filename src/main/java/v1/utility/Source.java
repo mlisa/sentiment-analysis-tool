2f74354a68db2d826944c204992cea90ffe679c8
lisamazzini <lisa.mazzini5@gmail.com>
2016-08-12 09:45:17 +0200

    added documentation; minor fixes; add enum for sources

---
+++ src/main/java/v1/utility/Source.java
@@ -0,0 +1,15 @@
+package v1.utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class of constants (
+ *
+ * TODO: enum?
+ *
+ */
+public enum Source {
+
+    TWITTER, FACEBOOK, INSTAGRAM,
+}
